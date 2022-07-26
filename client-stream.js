68531f957e0251be2862524795dc1cea0ca1506e
Erfan Yousefi <erfanyousefi.co@gmail.com>
2022-07-26 15:21:56 +0430

    some changes

--- client-stream.js
+++ client-stream.js
@@ -9,49 +9,34 @@ const INPUT =
   "http://localhost:3600";
 
 app.get("/audio", (req, res) => {
-    let stat = fs.statSync(music);
-    let range = req.headers.range;
-    let readStream;
-    if (range !== undefined) {
-        let parts = range.replace(/bytes=/, "").split("-");
-        let partial_start = parts[0];
-        let partial_end = parts[1];
-        if ((isNaN(partial_start) && partial_start.length > 1) || (isNaN(partial_end) && partial_end.length > 1)) {
-            return res.sendStatus(500);         
-        }
-
-        let start = parseInt(partial_start, 10);
-        let end = partial_end ? parseInt(partial_end, 10) : stat.size - 1;
-        let content_length = (end - start) + 1;
-        axios
-          .get(INPUT, {
-            responseType: "stream",
-            adapter: httpAdapter,
-            "Content-Range": `bytes ${start}-${end}`,
-          })
-          .then((Response) => {
-            const stream = Response.data;
-            res.set("content-type", "audio/mp3");
-            res.set("accept-ranges", "bytes");
-            res.set("content-length", Response.headers["content-length"]);
-            console.log(Response);
-      
-            stream.on("data", (chunk) => {
-              res.write(chunk);
-            });
-      
-            stream.on("error", (err) => {
-              res.sendStatus(404);
-            });
-      
-            stream.on("end", () => {
-              res.end();
-            });
-          })
-          .catch((Err) => {
-            console.log(Err.message);
-          });
-    }
+    axios
+      .get(INPUT, {
+        responseType: "stream",
+        adapter: httpAdapter,
+        "Content-Range": `bytes 16561-8065611`,
+      })
+      .then((Response) => {
+        const stream = Response.data;
+        res.set("content-type", "audio/mp3");
+        res.set("accept-ranges", "bytes");
+        res.set("content-length", Response.headers["content-length"]);
+        console.log(Response);
+  
+        stream.on("data", (chunk) => {
+          res.write(chunk);
+        });
+  
+        stream.on("error", (err) => {
+          res.sendStatus(404);
+        });
+  
+        stream.on("end", () => {
+          res.end();
+        });
+      })
+      .catch((Err) => {
+        console.log(Err.message);
+      });
 });
 
 app.listen(4000, () => {
